e8982fe987eb988cbac1510e3125ac73de0e1279
azirius <me@aziri.us>
2015-02-10 23:05:57 +0000

    changes to composer.json

--- src/LockdownAdminServiceProvider.php
+++ src/LockdownAdminServiceProvider.php
@@ -5,16 +5,31 @@ use Illuminate\Support\ServiceProvider;
 
 class LockdownAdminServiceProvider extends ServiceProvider
 {
+    /**
+     * Indicates if loading of the provider is deferred
+     * 
+     * @var boolean
+     */
+    protected $defer    =   false;
+
+    /**
+     * Bootstrap the application events
+     * 
+     * @return void 
+     */
     public function boot()
     {
         require __DIR__ . '/routes.php';
         $this->loadViewsFrom(__DIR__ . '/views/', 'lockdown');
     }
 
+    /**
+     * Register the service provider
+     * 
+     * @return void 
+     */
     public function register()
     {
-        $this->app->bind('Reflex\Lockdown\Lockdown', $this->app['lockdown']);
-    
         $this->publishes(
             [__DIR__ . '/../public/adminlte'   =>  base_path('/public/adminlte')],
             'public'
