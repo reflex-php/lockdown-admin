05b59cd7d3ffa4b211643269e2a6b04d70cb3a2b
azirius <me@aziri.us>
2015-02-11 12:17:56 +0000

    fixed bug in RoleController

--- src/Http/Controllers/RoleController.php
+++ src/Http/Controllers/RoleController.php
@@ -1,5 +1,8 @@
 <?php namespace Reflex\LockdownAdmin\Http\Controllers;
 
+use Reflex\LockdownAdmin\Http\Requests\Role as RoleRequest;
+use Reflex\Lockdown\Lockdown
+
 class RoleController extends BaseController
 {
     public function index()
@@ -15,8 +18,18 @@ class RoleController extends BaseController
         return view('lockdown::pages.create-role');
     } 
 
-    public function store()
+    public function store(RoleRequest $roleRequest)
     {
+        $result =   $this->lockdown->createRole(
+            $roleRequest->name,
+            snake_case($roleRequest->name),
+            $roleRequest->description
+        );
+
+        if ($result) {
+            return redirect()->route('lockdown.roles.index');
+        }
+
         return redirect()->back()->withInput();
     }
 }
