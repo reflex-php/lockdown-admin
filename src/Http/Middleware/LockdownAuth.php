44d4c78c2efe47feb64f775ae5a1950c7d1f772f
azirius <me@aziri.us>
2015-02-14 00:23:04 +0000

    Changes to authorisation

--- src/Http/Middleware/LockdownAuth.php
+++ src/Http/Middleware/LockdownAuth.php
@@ -6,15 +6,31 @@ use Closure;
 use Illuminate\Contracts\Routing\Middleware;
 use Config;
 use Auth;
+use Illuminate\Contracts\Auth\Guard as GuardContract;
 
 class LockdownAuth implements Middleware
 {
+    /**
+     * Auth instance
+     * @var \Illuminate\Contracts\Auth\Guard
+     */
+    protected $guard;
+
+    /**
+     * Class Constructor
+     * @param \Illuminate\Contracts\Auth\Guard $guard Auth instance
+     */
+    public function __construct(GuardContract $guard)
+    {
+        $this->guard    =   $guard;
+    }
+
     public function handle($request, Closure $next)
     {
-        $rolesRequired      =   Config::get('lockdown.admin.roles');
-        $allRequired        =   Config::get('lockdown.admin.all');
-        $afterFailedAuthUrl =   Config::get('lockdown.admin.login-route');
-        if (Auth::is($rolesRequired, $allRequired)) {
+        $rolesRequired      =   config('lockdown.admin.roles');
+        $allRequired        =   config('lockdown.admin.all');
+        $afterFailedAuthUrl =   config('lockdown.admin.login-route');
+        if ($this->auth->is($rolesRequired, $allRequired)) {
             return $next($request);
         }
 
