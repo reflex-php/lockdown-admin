03530ac5866cf46eade92e278c2bca4722a220e1
azirius <me@aziri.us>
2015-05-20 22:10:51 +0100

    bug fixes - mike

--- src/Reflex/LockdownAdmin/LockdownAdminServiceProvider.php
+++ src/Reflex/LockdownAdmin/LockdownAdminServiceProvider.php
@@ -44,7 +44,7 @@ class LockdownAdminServiceProvider extends ServiceProvider
         );
 
         $this->publishes(
-            [__DIR__ . '/views'   =>  base_path('/resources/views')],
+            [__DIR__ . '/views'   =>  base_path('resources/views/vendor/lockdown')],
             'views'
         );
     }
@@ -70,45 +70,22 @@ class LockdownAdminServiceProvider extends ServiceProvider
      */
     protected function registerLockdownAdminConfiguration()
     {
-        // Are we running an old or legacy version of Laravel?
-        if ($this->isLegacyLaravel() || $this->isOldLaravel()) {
-            $this->package('reflex/lockdown-admin', 'lockdown', __DIR__);
-        } else {
-            $this->publishes(
-                [
-                    __DIR__ . '/config/lockdown-admin.php'    =>  
-                        config_path('lockdown-admin.php'),
-                ],
-                'config'
-            );
+        $this->publishes(
+            [
+                __DIR__ . '/config/lockdown-admin.php'    =>  
+                    config_path('lockdown-admin.php'),
+            ],
+            'config'
+        );
 
-            $this->mergeConfigFrom(
-                __DIR__ . '/config/lockdown-admin.php', 
-                'lockdown.admin'
-            );
-        }
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/lockdown-admin.php', 
+            'lockdown.admin'
+        );
     }
 
     public function isAliasLoaded($alias)
     {
         return array_key_exists($alias, AliasLoader::getInstance()->getAliases());
-    }
-
-    /**
-     * Is the application running a legacy version of Laravel?
-     * @return boolean 
-     */
-    public function isLegacyLaravel()
-    {
-        return Str::startsWith(Application::VERSION, array('4.1.', '4.2.'));
-    }
-
-    /**
-     * Is the application running an old version of Laravel?
-     * @return boolean 
-     */
-    public function isOldLaravel()
-    {
-        return Str::startsWith(Application::VERSION, '4.0.');
     }
 }
